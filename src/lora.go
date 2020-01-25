0d19b539c97c0a8716684a0ea948990dae21b0f8
Alex Trottier <postables@rtradetechnologies.com>
2020-01-24 16:23:03 -0800

    more updatesg

--- src/lora.go
+++ src/lora.go
@@ -16,20 +16,13 @@ var (
 )
 
 func main() {
-	/*selectReceiver := func(pin rpio.Pin) {
-		pin.Low()
-	}
-	unselectReceiver := func(pin rpio.Pin) {
-		pin.High()
-	}*/
 	readReg := func(addr byte, pin rpio.Pin) byte {
 		log.Println("running select receiver")
-		//selectReceiver(pin)
+		pin.High()
 		var spibuf = []byte{addr & 0x7F, 0x00}
 		log.Println("transmitting spi data")
-		rpio.SpiTransmit(spibuf[0], spibuf[1])
 		rpio.SpiExchange(spibuf)
-		//unselectReceiver(pin)
+		pin.Low()
 		return spibuf[1]
 	}
 	if err := rpio.Open(); err != nil {
@@ -47,22 +40,22 @@ func main() {
 	pinSS := rpio.Pin(ssPin)
 	pinDIO := rpio.Pin(dio0)
 	pinRST := rpio.Pin(RST)
-	/*pinSS.Output()
+	pinSS.Output()
 	pinDIO.Output()
 	pinRST.Output()
-	*/
+
 	// setup spi
 	log.Println("setting up spi")
 
 	rpio.SpiSpeed(500000)
 
-	/*// setup lora
+	// setup lora
 	log.Println("setting up lora")
 	pinRST.High()
 	time.Sleep(time.Millisecond * 100)
 	pinRST.Low()
 	time.Sleep(time.Millisecond * 100)
-	*/
+
 	log.Println("reading version")
 	var sx1272, sx1276 bool
 	version := readReg(byte(REG_VERSION), pinSS)
